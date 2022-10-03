a92ef1aaa6b7b64f1b8d56aabd8c44420a87a1ca
farzyahoo <farzyahoo@gmail.com>
2022-10-03 19:25:56 +0700

    Produk

---
+++ oop/produk.php
@@ -0,0 +1,46 @@
+<?php
+
+
+class Produk
+{
+
+  public $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0;
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Assassin Creed";
+// $produk2->tambahProperty = "hahahaah";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+// echo "Komik : $produk3->penulis, $produk3->penerbit";
+// echo  "<br>";
+// echo $produk3->getLabel();
+
+
+$produk4 = new Produk();
+$produk4->judul = "Assassin Creed";
+$produk4->penulis = "Corey May";
+$produk4->penerbit = "Ubisoft";
+$produk4->harga = 300000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
